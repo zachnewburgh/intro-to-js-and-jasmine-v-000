5f504207578b640b8c4c214f781a786b1e044e41
zachnewburgh <zach.newburgh@gmail.com>
2016-03-28 16:40:54 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(flavor) {
+  return "I love " + flavor;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
